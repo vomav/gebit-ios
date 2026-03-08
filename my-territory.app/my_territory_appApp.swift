6279b5ce31130968c2b9c467a07b0f0b29d4a929
voytovychv <voytovychv@gmail.com>
2026-03-08 17:04:35 +0100

    Initial Commit

---
+++ my-territory.app/my_territory_appApp.swift
@@ -0,0 +1,17 @@
+//
+//  my_territory_appApp.swift
+//  my-territory.app
+//
+//  Created by Volodymyr Voytovych on 08.03.26.
+//
+
+import SwiftUI
+
+@main
+struct my_territory_appApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
